20f59948631df842b52ef4868b09cd504d4df49f
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-03-02 18:12:47 +0900

    [Submit] ouyou kadai 3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+	has_many :book_comments
+	has_many :favorites
 	belongs_to :user, optional: true
 	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
 	#presence trueは空欄の場合を意味する。
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :favorites, only: [:create,:destroy]
+  post 'favorites/:id', to: "favorites#create"
+  resources :book_comments, only: [:create,:destroy]
   root 'home#top'
   get 'home/about'
 end
