391a936329b2e2a17cf1dbbac3183214e8abecc5
dawasa5656 <dawasa5656@gmail.com>
2020-07-26 15:16:41 +0900

    commit2

---
+++ Desktop/work/laravel/test/storage/framework/views/ed60939e8ead809d392f3f7e71cea8d3d0d895d1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>about</title>
+</head>
+<body>
+	<h1>About</h1>
+</body>
+</html><?php /**PATH C:\Users\Shouichi\Desktop\work\laravel\test\resources\views/pages/about.blade.php ENDPATH**/ ?>
